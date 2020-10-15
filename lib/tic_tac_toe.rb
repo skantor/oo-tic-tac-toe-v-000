bef3b3a32969aed7ea38b5dda4ec9866ab366087
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-15 21:20:43 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,28 @@
+class TicTacToe
+  
+  def initialize
+    
+  end
+  
+  def display_board
+    puts @board
+  end
+  
+  def input_to_index
+    put @board
+  end
+  
+  def valid_move?
+    true
+  end
+  
+  def turn 
+    puts "Please provide a new move"
+  end
+  
+  WIN_COMBINATIONS = [
+    [0,1,2]
+    [3,4,5]
+    ]
+    
+end
